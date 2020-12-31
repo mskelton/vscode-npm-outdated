da3ce316ffcfa28b231c9f5952358ac3cf371053
Kang Dels <crushfire2004@gmail.com>
2020-12-31 17:12:53 +0700

    feat: add comparisson based on vscode setting

--- src/diagnostics/getPackageInfo.ts
+++ src/diagnostics/getPackageInfo.ts
@@ -1,14 +1,23 @@
 import semverCoerce from "semver/functions/coerce"
-import semverGt from "semver/functions/gt"
+import semverDiff from "semver/functions/diff"
+import { workspace } from 'vscode';
 import { fetchPackage } from "../utils/packages"
 
 export async function getPackageInfo([name, version]: string[]) {
   const info = await fetchPackage(name)
   const localVersion = semverCoerce(version)
+  const versionToCompare = workspace.getConfiguration().get<string>('npm-outdated.version')
+  const versionDiff = info && localVersion && semverDiff(info.version, localVersion)
+
+  const versionMap: any = {
+    major: 2,
+    minor: 1,
+    patch: 0
+  }
 
   return {
     latestVersion: info?.version,
     name,
-    outdated: info && localVersion && semverGt(info.version, localVersion),
+    outdated: versionDiff && versionToCompare && (versionMap[versionDiff] >= versionMap[versionToCompare]),
   }
 }
