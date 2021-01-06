fc879afb9b03f560662059dfb548257aaedb10a4
Mark Skelton <mdskelton99@gmail.com>
2021-01-05 19:12:11 -0600

    feat: Add setting to specify semver level

--- src/diagnostics/getPackageInfo.ts
+++ src/diagnostics/getPackageInfo.ts
@@ -1,23 +1,29 @@
 import semverCoerce from "semver/functions/coerce"
 import semverDiff from "semver/functions/diff"
-import { workspace } from 'vscode'
+import { workspace } from "vscode"
 import { fetchPackage } from "../utils/packages"
 
 export async function getPackageInfo([name, version]: string[]) {
   const info = await fetchPackage(name)
   const localVersion = semverCoerce(version)
-  const versionToCompare = workspace.getConfiguration().get<string>('npm-outdated.version')
-  const versionDiff = info && localVersion && semverDiff(info.version, localVersion)
+  const versionToCompare = workspace
+    .getConfiguration()
+    .get<string>("npm-outdated.version")
+  const versionDiff =
+    info && localVersion && semverDiff(info.version, localVersion)
 
   const versionMap: Record<string, number> = {
     major: 2,
     minor: 1,
-    patch: 0
+    patch: 0,
   }
 
   return {
     latestVersion: info?.version,
     name,
-    outdated: versionDiff && versionToCompare && (versionMap[versionDiff] >= versionMap[versionToCompare]),
+    outdated:
+      versionDiff &&
+      versionToCompare &&
+      versionMap[versionDiff] >= versionMap[versionToCompare],
   }
 }
