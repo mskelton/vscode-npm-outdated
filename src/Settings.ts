6dcc11eb994b7b882a6f0add60baa9f1073c8342
David Rodrigues <david.rodrigues@rentalhost.net>
2022-12-12 23:40:03 -0300

    fix: Misconfigured cache
    The cache system interpreted the "60" setting as "60ms" instead of "60 minutes".

--- src/Settings.ts
+++ src/Settings.ts
@@ -2,9 +2,13 @@ import { ReleaseType } from "semver"
 import { workspace } from "vscode"
 
 export const getCacheLifetime = () => {
-  return workspace
-    .getConfiguration()
-    .get<number>("npm-outdated.cacheLifetime") as number
+  return (
+    Number(
+      workspace.getConfiguration().get<number>("npm-outdated.cacheLifetime")
+    ) *
+    60 *
+    1000
+  )
 }
 
 export const getLevel = () => {
